602b7922c64d94b256549a1f8f29934603c7d329
Ben <131743470+benBUV@users.noreply.github.com>
2024-05-18 05:46:21 +0700

    Update and rename 1.1 to 1.1.py

---
+++ chapter1/1.1.py
@@ -0,0 +1,16 @@
+"""
+
+  Instructions:
+  Complete the program with five print statements to output the following figure with asterisks.
+  
+  *****
+  *   *
+  *****
+  *   *
+  *****
+  
+  Note: Whitespace (blank spaces/blank lines) matters; make sure your whitespace matches exactly the expected output.
+  
+"""
+
+# Your solution goes here
